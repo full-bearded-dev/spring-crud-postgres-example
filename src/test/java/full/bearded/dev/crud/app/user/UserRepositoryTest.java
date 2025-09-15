c8cc30092687ce846acf89084f0c37bcf8980d7c
full-bearded-dev <fullbeardeddev@gmail.com>
2025-09-15 22:08:08 +0100

    creating user JPA entity and repo

---
+++ src/test/java/full/bearded/dev/crud/app/user/UserRepositoryTest.java
@@ -0,0 +1,26 @@
+package full.bearded.dev.crud.app.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void userRepositoryTest() {
+
+        final var user = new User(null, "fbd", "fbd@example.com");
+        final var savedUser = userRepository.save(user);
+
+        final var foundUserList = userRepository.findAll();
+
+        assertThat(foundUserList).hasSize(1);
+        assertThat(foundUserList.get(0)).isEqualTo(savedUser);
+    }
+}
