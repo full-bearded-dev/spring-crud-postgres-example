264c5a0cee6e35cc2262b0402d5f59bf5e323886
full-bearded-dev <fullbeardeddev@gmail.com>
2025-09-15 22:09:02 +0100

    creating user JPA entity and repo (#2)

---
+++ src/main/java/full/bearded/dev/crud/app/user/User.java
@@ -0,0 +1,57 @@
+package full.bearded.dev.crud.app.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    public User(final Long id, final String name, final String email) {
+
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public User() {}
+
+    public Long getId() {
+
+        return id;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(final String name) {
+
+        this.name = name;
+    }
+
+    public String getEmail() {
+
+        return email;
+    }
+
+    public void setEmail(final String email) {
+
+        this.email = email;
+    }
+}
