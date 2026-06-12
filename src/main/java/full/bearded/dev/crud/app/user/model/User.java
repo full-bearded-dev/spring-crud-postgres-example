6bf5693cb46e98150cd59c3f06538a4b3e112591
full-bearded-dev <fullbeardeddev@gmail.com>
2026-06-12 23:17:52 +0100

    add liquibase database migration and fix tests

--- src/main/java/full/bearded/dev/crud/app/user/model/User.java
+++ src/main/java/full/bearded/dev/crud/app/user/model/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -23,7 +24,7 @@ import lombok.Setter;
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
